fabe2d387f3bd7c3b345777cf4e48e4b2ba8b70f
Lucia De Mojà <luciademoja@gmail.com>
2016-08-09 17:58:24 +0200

    added functionality to switch between imperial and metric system

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,14 @@ angular.module('starter.controllers', [])
 
 .controller('BmiController', function($scope) {
   $scope.data = {};
+  $scope.data.calc_type = 'metric';
   $scope.calculateBMI = function() {
     var person = new Person({
       weight: $scope.data.weight,
-      height: $scope.data.height
+      height: $scope.data.height,
+      calc_type: $scope.data.calc_type
     });
-    person.calculate_bmi_met();
+    person.calculate_bmi();
     $scope.person = person;
   };
 })
